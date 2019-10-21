b351f7f46d8bc6bb44cd5e87cfbe8d17cc00de80
Gergely Nemeth <gnemeth@uber.com>
2019-10-21 15:27:53 -0700

    docs(survey): csat

--- documentation-site/components/survey.js
+++ documentation-site/components/survey.js
@@ -6,7 +6,11 @@ LICENSE file in the root directory of this source tree.
 */
 // @flow
 /* eslint-env browser */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import CookiesConstructor from 'universal-cookie';
+
+import {trackEvent} from '../helpers/ga';
+
 import {useStyletron} from 'baseui';
 import {StarRating} from 'baseui/rating';
 import {Label2} from 'baseui/typography';
@@ -15,7 +19,16 @@ import Delete from 'baseui/icon/delete';
 const Survey = () => {
   const [useCss, theme] = useStyletron();
   const [rating, setRating] = useState();
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const {showSurvey, delay} = shouldShowSurvey();
+    if (!showSurvey) return;
+    setTimeout(() => {
+      setIsOpen(showSurvey);
+    }, delay);
+  }, []);
+
   return isOpen ? (
     <div
       className={useCss({
@@ -26,7 +39,8 @@ const Survey = () => {
         width: '100%',
         boxShadow: theme.lighting.shadow400,
         textAlign: 'center',
-        padding: theme.sizing.scale800,
+        paddingTop: theme.sizing.scale800,
+        paddingBottom: theme.sizing.scale800,
         backgroundColor: theme.colors.background,
         [`@media screen and (max-width: ${theme.breakpoints.medium}px`]: {
           display: 'none',
@@ -44,6 +58,7 @@ const Survey = () => {
         numItems={5}
         onChange={data => {
           setRating(data.value);
+          trackEvent('survey', 'csat', data.value);
           window.open(`
             https://docs.google.com/forms/d/e/1FAIpQLSfQ6uxhW96LX31x5hbp2xIb-WPI0eIavfAb_s7lkhKnz-LIJQ/viewform?usp=pp_url&entry.1493106267=${data.value}
           `);
@@ -52,11 +67,18 @@ const Survey = () => {
         value={rating}
       />
       <div
+        role="button"
+        tabIndex="0"
+        onKeyPress={event => {
+          if (event.key === 'Enter') {
+            setIsOpen(false);
+          }
+        }}
         className={useCss({
           cursor: 'pointer',
           position: 'absolute',
           top: theme.sizing.scale400,
-          right: theme.sizing.scale1600,
+          right: theme.sizing.scale400,
         })}
         onClick={() => {
           setIsOpen(false);
@@ -67,5 +89,55 @@ const Survey = () => {
     </div>
   ) : null;
 };
+
+// we want to show the survey once every two months
+function shouldShowSurvey() {
+  try {
+    // forcing the display of the survey for testing purposes
+    const {search} = window.location;
+    if (search && search.includes('survey')) {
+      return {showSurvey: true, delay: 0};
+    }
+  } catch (err) {}
+
+  const Cookies = new CookiesConstructor();
+
+  const cookies = {
+    FIRST_SEEN: 'survey-user-first-seen',
+    LAST_SURVEYED: 'survey-last-surveyed',
+  };
+  const TWO_MONTHS_AGO = 60 * 24 * 60 * 60 * 1000;
+  const ONE_WEEK_AGO = 7 * 24 * 60 * 60 * 1000;
+  const SURVEY_DELAY = 2 * 60 * 1000;
+
+  const firstSeen = Cookies.get(cookies.FIRST_SEEN);
+  const lastSurveyed = Cookies.get(cookies.LAST_SURVEYED);
+  const now = new Date();
+
+  // the person is a new visitor, bailing out
+  if (!firstSeen) {
+    Cookies.set(cookies.FIRST_SEEN, now.getTime(), {
+      // by default, cookies are session cookies, so without an expiration they are removed
+      // when the browser window is closed.
+      expires: new Date(now.getFullYear() + 10, now.getMonth()),
+    });
+    return {showSurvey: false};
+  }
+
+  // the person only knows base web for less than a week
+  if (firstSeen > ONE_WEEK_AGO) {
+    return {showSurvey: false};
+  }
+
+  // the person was surveyed less than two months ago, bailing out
+  if (lastSurveyed > TWO_MONTHS_AGO) {
+    return {showSurvey: false};
+  }
+
+  Cookies.set(cookies.LAST_SURVEYED, now.getTime(), {
+    expires: new Date(now.getFullYear() + 10, now.getMonth()),
+  });
+  return {showSurvey: true, delay: SURVEY_DELAY};
+}
 
 export default Survey;
